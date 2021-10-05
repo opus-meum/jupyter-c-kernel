476fb24eaae3da4c0c575647424b514de20c1f50
opus-meum <james@opusmeum.dev>
2021-10-05 12:45:00 +0200

    Change to opus-meum

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jupyter_c_kernel',
-      version='1.2.1',
+      version='1.2.3',
       description='Minimalistic C kernel for Jupyter',
       author='Brendan Rius',
       author_email='ping@brendan-rius.com',
@@ -9,8 +9,8 @@ setup(name='jupyter_c_kernel',
       classifiers=[
           'License :: OSI Approved :: MIT License',
       ],
-      url='https://github.com/brendan-rius/jupyter-c-kernel/',
-      download_url='https://github.com/brendan-rius/jupyter-c-kernel/tarball/1.2.1',
+      url='https://github.com/opus-meum/jupyter-c-kernel/',
+      download_url='https://github.com/opus-meum/jupyter-c-kernel/tarball/1.2.3',
       packages=['jupyter_c_kernel'],
       scripts=['jupyter_c_kernel/install_c_kernel'],
       keywords=['jupyter', 'notebook', 'kernel', 'c'],
